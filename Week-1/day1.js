5204b5c5a24cc12aee58b7bc140e995fe3680b02
Keith Kimani <xavierkimani06@gmail.com>
2024-01-22 13:27:26 +0300

    initial commit

---
+++ Week-1/day1.js
@@ -0,0 +1,60 @@
+/**
+ *@var
+ *You can redeclare a variable
+ * var name = "Ryan"
+ * var name = "Judy"
+ 
+ *You can reassign a variable
+ *var name = "Ryan"
+ *name = "Judy"
+ 
+ *@let
+ *You can redclare a variable
+ *let name = "Ryan"
+ *let name = "Judy"
+
+ *You can't ressign a variable
+ *let name = "Ryan"
+ *let name = "Judy"
+
+ *@const 
+ *You can't redeclare a variable
+ *const name = "Ryan"
+ *const name = "Judy"
+
+ *You can't reassign a variable
+ *const name = "Ryan"
+ *const name = "Judy"
+
+*/
+
+//DATA TYPES
+ /**Primitive Data Types
+  * @Strings - Characters/numbers wrapped in quotes --> "Dan", "123"
+  * @Numbers - Decimal, whole numbers, nagative numbers --> 1, 2, 3, 1.5, -7
+  * @booleans - True or false
+  * @undefined - No value assigned
+  * @Null - Nothing is input
+*/
+/**Non-primitive Data Types
+ * @lists - Arrays --> [1, 2, 3, 4], [Dan, Nigel, Sandra]
+ * @objects - Dictionaries (key/values pairs)
+*/
+
+//OPERATES
+/**
+ * @Addiction --> (+)
+ * @Subtraction --> (-)
+ * @Multiplication --> (*)
+ * @Division --> (/)
+ * @Modulus --> (%)
+*/
+
+//Revenue
+//Expenses
+//Cost of goods sold
+//Profit
+
+const profit = 20000 - 500 -5000
+
+console.log(profit)
