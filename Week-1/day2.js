a357d5122852cf2769473adbdd32f5687a59d43b
Keith Kimani <xavierkimani06@gmail.com>
2024-01-23 12:28:06 +0300

    day2.js

---
+++ Week-1/day2.js
@@ -0,0 +1,48 @@
+let school = "Zindua"
+
+//if(school === "Zindua") {
+  //  console.log("Zindua School")
+//} else { 
+  //  console.log("Not Zindua School")
+//}
+
+/**
+ * @LOGICALOPERATORS
+ * @STRICTLYEQUAL --> ==
+ * @AND --> &&
+ * @OR --> //
+ * @GREATERTHAN --> (>)
+ * @LESSTHAN --> (<)
+ * @GREATERTHANOREQUAL --> (>=)
+ * @LESSTHANOREQUAL --> (<=)
+ * @NOT --> (!)
+ * @NOTEQUAL --> (!==)
+*/
+
+//let name = "James"
+
+//switch (name) {
+    //case "James":
+      //  console.log("I am James")
+        //break;
+    //case "Kamau":
+      //  console.log("I am Kamau")
+        //break;
+    //default: 
+    console.log("I am not James or Kamau")   
+   // break;
+
+//}
+
+//function consoleHello() { //function declaration
+  //  console.log ('Hello')//function body 
+//}
+
+//consoleHello()//function call/invocation (must be there for the program to run)
+
+
+//FOR LOOPS
+//let number = 10
+
+//for(let i 10; 1<=number; 1++)
+ //console.log(i)
